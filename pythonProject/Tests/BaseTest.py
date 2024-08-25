964c89f6b13f6b99de25382b9ab545820c5aa8a9
farosep <you@example.com>
2024-08-25 15:30:11 +0700

    add main files

--- pythonProject/Tests/BaseTest.py
+++ pythonProject/Tests/BaseTest.py
@@ -1,9 +1,15 @@
 from selenium import webdriver
+import pytest
 
 
 class BaseTest:
     def __init__(self):
         self.driver = webdriver.Chrome()
+
+    @pytest.fixture
+    def arrange(self):
+        pass
+        # тут какие то действия по подготовке окружения
 
     def go_to_page(self, url:str):
         self.driver.get(url)
