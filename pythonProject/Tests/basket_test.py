a334d23091dfc42a0a0200882c273209156fa0cf
avysotskiy <avysotskiy@umschool.ru>
2024-08-26 22:19:11 +0700

    final

--- pythonProject/Tests/basket_test.py
+++ pythonProject/Tests/basket_test.py
@@ -10,13 +10,15 @@ class TestBasket(BaseTest):
         #Act
         main_page.open_page()
         main_page.select_parents_radio()
-        catalog_page = main_page.select_11_class()
+        catalog_page = main_page.select_11_class() # не не может выполниться
         catalog_page.wait_until_loaded()
-        catalog_page.button_profile_math.scroll_to_element()
         catalog_page.select_profil_math()
-        catalog_page.select_sharafiev_course()
+        course_page = catalog_page.select_sharafiev_course()
+        course_page.wait_until_loaded()
+        course_page.add_course_to_basket() # Не отрабатывает
+        basket_page = course_page.go_to_basket()
 
+        # Assert
+        basket_page.check_product_is_added() # продукт не добавился, а проверка прошла
 
-        pass
-        #Assert
 
--- pythonProject/pages/Catalog.py
+++ pythonProject/pages/Catalog.py
@@ -1,3 +1,5 @@
+from selenium.webdriver.common.by import By
+from pages.SharafievPage import SharafievPage
 
 from elements.BaseElement import BaseElement
 from elements.Button import Button
@@ -5,11 +7,11 @@ from pages.BasePage import BasePage
 
 
 class CatalogPage(BasePage):
-    base_math_selector = "[data-scroll-item=\"3\"]"
-    profile_math_selector = "#__nuxt > main > div > div.ui-radius-md.ums-card.ums-component-root.ui-bg-grey-0.ui-px-3.ui-py-4.sm\:ui-p-4.md\:ui-p-5.lg\:ui-p-8.space-y-8.sm\:space-y-10.lg\:space-y-20.my-8.md\:my-20 > div > div.mb-9.sm\:mb-10.md\:mb-14.lg\:mb-20 > div > div.base-slider.base-slider--with-offset.base-slider--full-height > div.base-slider__area.no-scrollbar > div:nth-child(3) > div"
-    russian_selector = "[data-scroll-item=\"1\"]"
-    all_subjects_selector = "#__nuxt > main > div > div.ui-radius-md.ums-card.ums-component-root.ui-bg-grey-0.ui-px-3.ui-py-4.sm\:ui-p-4.md\:ui-p-5.lg\:ui-p-8.space-y-8.sm\:space-y-10.lg\:space-y-20.my-8.md\:my-20 > div > div.mb-9.sm\:mb-10.md\:mb-14.lg\:mb-20 > div > div.base-slider.base-slider--with-offset.base-slider--full-height > div.base-slider__area.no-scrollbar > div:nth-child(1)"
-    profile_math_sharafiev_locator = "[data-testid=\"PRODUCT_CARD\"] a[href=\"/ege/math-11-class-artur-sharafiev-8608/\"]"
+    base_math_selector = By.CSS_SELECTOR, "[data-scroll-item=\"3\"]"
+    profile_math_selector = By.CSS_SELECTOR, "div.mb-9.sm\\:mb-10.md\\:mb-14.lg\\:mb-20 > div > div.base-slider.base-slider--with-offset.base-slider--full-height > div.base-slider__area.no-scrollbar > div:nth-child(3) > div"
+    russian_selector = By.CSS_SELECTOR, "[data-scroll-item=\"1\"]"
+    all_subjects_selector = By.CSS_SELECTOR, "#__nuxt > main > div > div.ui-radius-md.ums-card.ums-component-root.ui-bg-grey-0.ui-px-3.ui-py-4.sm\:ui-p-4.md\:ui-p-5.lg\:ui-p-8.space-y-8.sm\:space-y-10.lg\:space-y-20.my-8.md\:my-20 > div > div.mb-9.sm\:mb-10.md\:mb-14.lg\:mb-20 > div > div.base-slider.base-slider--with-offset.base-slider--full-height > div.base-slider__area.no-scrollbar > div:nth-child(1)"
+    profile_math_sharafiev_locator = By.CSS_SELECTOR, "[data-testid=\"PRODUCT_CARD\"] a[href=\"/ege/math-11-class-artur-sharafiev-8608/\"]"
 
     def wait_until_loaded(self):
         self._wait_until_url_contains("/11-class")
@@ -19,6 +21,7 @@ class CatalogPage(BasePage):
 
     def select_sharafiev_course(self):
         self.button_sharafiev_cource.click()
+        return SharafievPage(self.driver)
 
     @property
     def button_base_math(self):
