5ea997d62c2585c2bce28d93375bfb5b420c6403
avysotskiy <avysotskiy@umschool.ru>
2024-08-26 13:32:18 +0700

    venv

---
+++ pythonProject/Tests/basket_test.py
@@ -0,0 +1,19 @@
+from pages.MainPage import MainPage
+from base_test import BaseTest
+
+
+class TestBasket(BaseTest):
+    def test_basket(self):
+        #Arrange
+        main_page = MainPage(self.driver)
+
+        #Act
+        main_page.open_page()
+        catalog_page = main_page.select_ege_11()
+        catalog_page.wait_until_loaded()
+
+        #хорошая идея будет сделать тут фиск этого косяка про фикстуры
+
+        pass
+        #Assert
+
