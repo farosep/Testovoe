37d6ec285aaa7d012c674b3e6be27f87d705fa21
avysotskiy <avysotskiy@umschool.ru>
2024-10-28 13:21:06 +0700

    update api task

--- Tests/API/booking_test.py
+++ Tests/API/booking_test.py
@@ -1,29 +1,7 @@
-import requests
-import random
-
 
 class TestBooking:
-    base_url = "https://restful-booker.herokuapp.com/"
 
-# TODO Починить нестабильный тест. Почему то иногда падает.
-    def test_get_by_id(self):
-        # Arrange
-        random_id = random.randint(100, 1000)
-        url = self.base_url + f"booking/{random_id}"
-
-        # Act
-        response = requests.get(url)
-        data = response.json()
-
-        # Assert
-        assert response.status_code == 200
-        assert isinstance(data["firstname"], str)
-        assert isinstance(data["lastname"], str)
-        assert isinstance(data["totalprice"], int)
-        assert isinstance(data["depositpaid"], bool)
-        assert isinstance(data["additionalneeds"], str)
-
-# TODO Написать параметризованный тест который проверит всё что нужно
+    # TODO Написать параметризованный тест который проверит всё что нужно
     def test_create_booking(self):
         pass
         # https://restful-booker.herokuapp.com/apidoc/index.html
