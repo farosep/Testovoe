20167dfb467366a771ae822ed54d3f737719d35c
avysotskiy <avysotskiy@umschool.ru>
2024-10-29 13:40:24 +0700

    api test fix

--- Tests/API/booking_test.py
+++ Tests/API/booking_test.py
@@ -1,9 +1,10 @@
 
 class TestBooking:
 
-    # TODO Написать параметризованный тест который проверит всё что нужно
+
+    # TODO Написать параметризованный тест который проверит все нужные позитивные проверки
     def test_create_booking(self):
         pass
-        # https://restful-booker.herokuapp.com/apidoc/index.html
+        # https://restful-booker.herokuapp.com/apidoc/index.html#api-Booking-CreateBooking
 
     # Со звёздочкой, сделать использование токена в посте
